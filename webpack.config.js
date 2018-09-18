78c8fb4023b024e89e35697689394f9bf7769ea8
Seth Donohue <sdonohue@paciolan.com>
2018-09-18 14:48:30 -0700

    added uglify and optimize to minimize css, added sass and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,13 +10,31 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional...
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    'style-loader',
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
                     'css-loader',
+                    'sass-loader',
                 ]
             }
         ]
